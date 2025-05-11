d5c83178439ee3ba318c5dd3144fd7f68f237903
airrick1985 <35885689+airrick1985@users.noreply.github.com>
2025-05-11 08:59:10 +0000

    2025-05-11 16:59:10

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 // src/main.js
 import { createApp } from 'vue'
-import App from './App.vue'     // 引入 App.vue
-import router from './router' // 引入你的路由設定
+import App from './App.vue'
+import router from './router' // 假設你有路由
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// 如果你需要特定的 icons (例如 Material Design Icons)
+import { aliases, mdi } from 'vuetify/iconsets/mdi' // 或者 fa (Font Awesome) 等
+import '@mdi/font/css/materialdesignicons.css' // 確保安裝了 @mdi/font
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: { // 如果使用 icons
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  // 你可以在這裡添加其他 Vuetify 配置，例如主題
+})
 
 const app = createApp(App)
-app.use(router)             // 使用路由
-app.mount('#app')           // 掛載到 index.html 中的 #app div
+
+app.use(router) // 使用路由
+app.use(vuetify) // 使用 Vuetify
+
+app.mount('#app')
